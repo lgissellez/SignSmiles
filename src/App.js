08803cab1837071c2ef968d9ef77834b1e7ec9a8
lgissellez <gssllzng@gmail.com>
2018-06-22 22:08:48 -0600

    SignSmiles
    SignSmiles

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">SignSmiles</h1>
+        </header>
+        <img src= "https://i.imgur.com/9OeHzzV.jpg" title="source: imgur.com" />
+        <p className="App-intro">
+          Bienvenidos a SignSmiles!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
